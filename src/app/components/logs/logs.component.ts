09d5b5656ef6af31e982d2aa36435595423b4b8d
Mike Peters <mike@petmi627.lu>
2018-07-31 16:53:23 +0200

    Hard Coded Log Elements

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./logs.component.css']
 })
 export class LogsComponent implements OnInit {
+  logs: {
+    id: string
+    text: string,
+    date: any
+  }[];
 
   constructor() { }
 
   ngOnInit() {
+    this.logs = [
+      {id: '1', text: 'Generated Components', date: new Date('07/29/2018 12:54:45')},
+      {id: '2', text: 'Added Bootstrap', date: new Date('07/29/2018 13:34:49')},
+      {id: '3', text: 'Added Logs Components', date: new Date('07/30/2018 09:24:05')},
+    ];
   }
 
 }
