a4a8056bd4be0de56e51c5647fa8b6f229e63d8d
Mike Peters <mike@petmi627.lu>
2018-08-07 20:26:06 +0200

    Added Local Storage

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -28,11 +28,22 @@ export class LogService {
   }
 
   getLogs(): Observable<Log[]> {
-    return of(this.logs);
+    if (localStorage.getItem('logs') === null) {
+        this.logs = [];
+    } else {
+        this.logs = JSON.parse(localStorage.getItem('logs'));
+    }
+
+    return of(this.logs.sort((a, b) => {
+        return b.date = a.date;
+    }));
   }
 
   addLog(log: Log) {
       this.logs.unshift(log);
+
+      // Add to local Storage
+      localStorage.setItem('logs', JSON.stringify(this.logs));
   }
 
   updateLog(log: Log) {
@@ -42,6 +53,7 @@ export class LogService {
          }
       });
       this.logs.unshift(log);
+      localStorage.setItem('logs', JSON.stringify(this.logs));
   }
 
   deleteLog(log: Log) {
@@ -50,6 +62,7 @@ export class LogService {
               this.logs.splice(index, 1);
           }
       });
+      localStorage.setItem('logs', JSON.stringify(this.logs));
   }
 
   setFormLog(log: Log) {
